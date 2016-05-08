88cac5ab58b29e9291fd3c56d032510501fc279a
Arnaud Cormier <arno.cormier@gmail.com>
2016-05-08 20:57:45 +0200

    testing js for slide invit and scrolldown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,23 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+$('.scroll-down').click(function(event) {
+    // Preventing default action of the event
+    event.preventDefault();
+    // Getting the height of the document
+    var n = $(document).height();
+    $('html, body').animate({ scrollTop: n }, 1500);
+    ;
+//                                       |    |
+//                                       |    --- duration (milliseconds)
+//                                       ---- distance from the top
+});
+$("#email").hide();
+$( ".clickme" ).click(function() {
+  $(".btn-danger").hide();
+  $( "#email" ).slideDown( "slow", function() {
+    // Animation complete.
+  });
+});
